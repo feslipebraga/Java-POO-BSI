0ec4530bda4ca1213a36c5bce21e1d91229d4280
feslipebraga <feslipebraga@gmail.com>
2024-03-06 11:24:39 -0300

    versão 3

---
+++ LojaGuitarra/src/v3/GuitarSpec.java
@@ -0,0 +1,89 @@
+package v3;
+
+public class GuitarSpec {
+	private Builder builder;
+    private String model;
+    private Type type;
+    private int numStrings;
+    private Wood backWood;
+    private Wood topWood;
+    
+	public GuitarSpec(Builder builder, String model, Type type, int numStrings, Wood backWood, Wood topWood) {
+		this.builder = builder;
+		this.model = model;
+		this.type = type;
+		this.numStrings = numStrings;
+		this.backWood = backWood;
+		this.topWood = topWood;
+	}
+	
+	public boolean matches(GuitarSpec otherSpec) {
+		if (builder != otherSpec.builder) {
+			return false;
+		}
+        if ((model != null) && (!model.equals("")) && (!model.equals(otherSpec.model))) {
+            return false;
+        }
+        if (type != otherSpec.type) {
+        	return false;
+        }
+        if (numStrings != otherSpec.numStrings) {
+        	return false;
+        }
+        if (backWood != otherSpec.backWood) {
+        	return false; 
+        }
+        if (topWood != otherSpec.topWood) {
+        	return false;
+        }
+        return true;
+	}
+
+	public Builder getBuilder() {
+		return builder;
+	}
+
+	public void setBuilder(Builder builder) {
+		this.builder = builder;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public int getNumStrings() {
+		return numStrings;
+	}
+	
+	public void setNumStrings(int numStrings) {
+		this.numStrings = numStrings;
+	}
+
+	public Wood getBackWood() {
+		return backWood;
+	}
+
+	public void setBackWood(Wood backWood) {
+		this.backWood = backWood;
+	}
+
+	public Wood getTopWood() {
+		return topWood;
+	}
+
+	public void setTopWood(Wood topWood) {
+		this.topWood = topWood;
+	}
+	
+}
---
+++ LojaGuitarra/src/v3/Type.java
@@ -0,0 +1,14 @@
+package v3;
+
+public enum Type { 
+
+    ACOUSTIC, ELECTRIC;
+  
+    public String toString() {
+      switch(this) {
+        case ACOUSTIC: return "acoustic";
+        case ELECTRIC: return "electric";
+        default:       return "unspecified";
+      }
+    }
+  }
