1c3dbb85a0aeca5ef7c4c07e61b78640c374304b
feslipebraga <feslipebraga@gmail.com>
2024-04-24 16:55:16 -0300

    Weather Monitoring Application

---
+++ WeatherMonitoring/src/app/DisplayElement.java
@@ -0,0 +1,6 @@
+package app;
+
+public interface DisplayElement {
+	public void display();
+	
+}
---
+++ WeatherMonitoring/src/app/Subject.java
@@ -0,0 +1,10 @@
+package app;
+
+public interface Subject {
+
+	public void registerObserver(Observer o);
+
+	public void removeObserver(Observer o);
+
+	public void notifyObservers();
+}
